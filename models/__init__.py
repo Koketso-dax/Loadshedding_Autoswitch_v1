d785a389283d5779f57d2b8f83692a7550ccaea3
Koketso-dax <lanchaster.k@gmail.com>
2024-05-27 16:17:34 +0200

    Updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
-"""
-Empty init
-"""
+from influxdb import InfluxDBClient
+from config import Config
+
+client = InfluxDBClient(host=Config.INFLUXDB_HOST, port=Config.INFLUXDB_PORT)
+client.switch_database(Config.INFLUXDB_DATABASE)
+
+from models.user import User
+from models.device import Device
