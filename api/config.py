58aec7a9db2b519ca07c00d330829f48664eb0dc
Koketso-dax <lanchaster.k@gmail.com>
2024-06-12 15:35:48 +0200

    update config

--- api/config.py
+++ api/config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://dev:password@localhost/loadshedding'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
-    INFLUXDB_ADDRESS = 'localhost'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret_key')
+    INFLUXDB_ADDRESS = 'web-01.koketsodiale.tech'
     INFLUXDB_PORT = 8086
-    INFLUXDB_DATABASE = 'mqtt_data'
-    MQTT_BROKER = 'localhost'
+    INFLUXDB_DATABASE = 'loadshedding'
+    MQTT_BROKER = 'web-01.koketsodiale.tech'
     MQTT_PORT = 1883
