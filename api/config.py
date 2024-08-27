a4b940394f09442822d9523c16c589e9123ef74c
Koketso-dax <lanchaster.k@gmail.com>
2024-08-27 15:47:53 +0200

    resolve test issues

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/autoswitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5433/autoswitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret_key')
     MQTT_BROKER = '127.0.0.1'
