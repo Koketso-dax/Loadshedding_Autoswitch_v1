c7ef4c4388538baa9008c98da0831c2a3b13918f
Koketso-dax <lanchaster.k@gmail.com>
2024-09-12 21:03:02 +0200

    Roll back

--- api/models/measurement.py
+++ api/models/measurement.py
@@ -1,63 +1,21 @@
 """
-    measurements.py
+    Python module for data hypertables
 """
-import psycopg2
+from models import db
 
-class Measurement:
-    def __init__(self, device_id, timestamp, power_measurement):
-        self.device_id = device_id
-        self.timestamp = timestamp
-        self.power_measurement = power_measurement
+
+class Measurement(db.Model):
+    """ Class definition for measurement Hypertable """
+    __tablename__ = 'measurements'
+    __table_args__ = {'__timescaledb_hypertable__': {'time_column': 'timestamp', 'chunk_time_interval': '1 day'}}
+
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.Integer, db.ForeignKey('devices.id'))
+    timestamp = db.Column(db.DateTime())
+    power_measurement = db.Column(db.Float)
+
+    device = db.relationship('Device', backref='device_measurements')
 
     def save(self):
-        conn = psycopg2.connect(
-            dbname="autoswitch",
-            user="postgres",
-            password="password",
-            host="localhost",
-            port="5433"
-        )
-        cur = conn.cursor()
-
-        # Check if the measurements table exists
-        cur.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'measurements')")
-        table_exists = cur.fetchone()[0]
-
-        if not table_exists:
-            # Create the measurements table with a foreign key constraint to the devices table
-            cur.execute("""
-                CREATE TABLE measurements (
-                    measurement_id SERIAL PRIMARY KEY,
-                    device_id INTEGER NOT NULL REFERENCES devices(device_id),
-                    timestamp TIMESTAMPTZ NOT NULL,
-                    power_measurement FLOAT NOT NULL
-                );
-            """)
-            conn.commit()
-
-        # Insert the measurement data into the measurements table
-        cur.execute("INSERT INTO measurements (device_id, timestamp, power_measurement) VALUES (%s, %s, %s);",
-                    (self.device_id, self.timestamp, self.power_measurement))
-        conn.commit()
-
-        cur.close()
-        conn.close()
-
-    @staticmethod
-    def create_continuous_aggregates():
-        conn = psycopg2.connect(
-            dbname="autoswitch",
-            user="postgres",
-            password="password",
-            host="localhost",
-            port="5433"
-        )
-        cur = conn.cursor()
-
-        # Populate the materialized views with data
-        cur.execute("REFRESH MATERIALIZED VIEW kwh_day_by_day")
-        cur.execute("REFRESH MATERIALIZED VIEW kwh_hour_by_hour")
-
-        conn.commit()
-        cur.close()
-        conn.close()
+        db.session.add(self)
+        db.session.commit()
--- api/services/mqtt_handler.py
+++ api/services/mqtt_handler.py
@@ -1,20 +1,18 @@
 """ MQTT Client Module"""
 import paho.mqtt.client as mqtt
+from models.device import Device
 from models.measurement import Measurement
+from models import db
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe("devices/#")
 
-    # Create continuous aggregates when the program starts up
-    Measurement.create_continuous_aggregates()
-
 def on_message(client, userdata, msg):
-    device_name = msg.topic.split('/')[1]
+    device_id = int(msg.topic.split('/')[1])
     data = msg.payload.decode('utf-8')
     timestamp, power_measurement = data.split(',')
-    measurement = Measurement(device_name=device_name, timestamp=timestamp,
-                              power_measurement=float(power_measurement))
+    measurement = Measurement(device_id=device_id, timestamp=timestamp, power_measurement=float(power_measurement))
     measurement.save()
 
 def init_mqtt_client(app):
@@ -22,7 +20,5 @@ def init_mqtt_client(app):
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
-    client.connect(app.config['MQTT_BROKER'],
-                   app.config['MQTT_PORT'], 60)
+    client.connect(app.config['MQTT_BROKER'], app.config['MQTT_PORT'], 60)
     client.loop_start()
